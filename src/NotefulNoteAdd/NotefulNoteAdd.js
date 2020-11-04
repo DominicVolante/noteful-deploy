53f2f116888c206fe2ad4c59a45931298a67ec32
Dominic Volante <dominicvolante@gmail.com>
2020-11-04 16:54:07 -0500

    first deploy commit

---
+++ src/NotefulNoteAdd/NotefulNoteAdd.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import ApiContext from '../ApiContext'
+import config from '../config';
+
+
+class NotefulNoteAdd extends Component {
+    static contextType = ApiContext
+    static defaultProps = {
+        addNote: () => { },
+      }
+
+      getFolderList = () => {
+        console.log(this.context)
+        let arr = this.context.folders.map(folder => {
+          return 
+        })
+        return 
+      }
+
+      getNoteDetails = (form) => {
+        const n = new FormData(form)
+        let name = n.get("noteName")
+        let content = n.get("noteContent")
+        let date = new Date().toString()
+        let foldId = 
+        console.log(this.getFolderList())
+        if (typeof name !== "string" || name.length <= 0) return;
+        // const folder = {name:name}
+        // console.log(folder)
+        this.handleSubmit({name:name, content:content, modified:date})
+    
+      }
+      handleSubmit = (data) => {
+        fetch(`${config.API_ENDPOINT}/notes/`, {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        })
+        .then(res => {
+            this.props.addNote(data)
+        this.props.history.push('/')
+      })
+      .catch(error => {
+        console.error({ error })
+      })
+    }
+    render() { 
+        const { className } = this.context
+        return (
+                <form
+                className={['Noteful-form', className].join(' ')}
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    this.getNoteDetails(e.target)
+
+                }}
+            >
+              <select name="folderName" id="folderName">
+
+              </select>
+                <input type="text" id="noteName" name="noteName" aria-label="noteName" placeholder="New Note" required />
+                <textarea type="text" id="noteContent" name="noteContent" placeholder="Note Content Here" required />
+                <button type="submit">Submit</button>
+            </form>
+          );
+    }
+}
+ 
+export default NotefulNoteAdd;
