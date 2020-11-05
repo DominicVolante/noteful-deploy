0f89ab9017abc0dd9767ae7536a8b19947a77bc0
Dominic Volante <dominicvolante@gmail.com>
2020-11-05 15:16:42 -0500

    server fixed

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://stormy-earth-36019.herokuapp.com/'
+  //API_ENDPOINT:'http://localhost:9090'
+  API_ENDPOINT: 'https://stormy-earth-36019.herokuapp.com',
 }
