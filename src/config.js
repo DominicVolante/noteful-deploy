7ddc7c58486b89fd9663b3f62091bbc6f0b4a196
Dominic Volante <dominicvolante@gmail.com>
2020-11-05 14:48:07 -0500

    real endpoint added

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-  API_ENDPOINT: 'postgres://dlaqnchpwksfpl:36e9542befc4b5234c8dc49b0cea8b2186bef6774e0c1ef47922acb0de4abf04@ec2-3-211-176-230.compute-1.amazonaws.com:5432/d6i5hplll8fgnj'
+  API_ENDPOINT: 'https://stormy-earth-36019.herokuapp.com/'
 }
